73c97b8c43ad1885abbc38702a5e6952a477a950
devboyd <devin4boyd@gmail.com>
2022-02-21 20:27:34 -0600

    started bonus mission

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,39 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @Size(min = 5, max = 15, message = "Don't leave blank")
+    @NotBlank(message = "Field required")
     private String username;
+
+    @Email(message = "must be valid email")
     private String email;
+
+    @NotBlank(message = "Field required")
+    @Size(min = 6, max = 20, message = "Don't leave blank")
     private String password;
 
-    public User() {
+    private String verifyPassword;
 
-    }
+    public User() {};
 
     public User(String username, String email, String password) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    private void checkPassword() {
+        if(password.equals(verifyPassword)) {
+
+        } else {
+
+        }
     }
 
     public String getUsername() {
@@ -38,5 +58,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
     }
 }
